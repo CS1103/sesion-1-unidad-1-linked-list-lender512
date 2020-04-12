10c09282d730a57f7e5ee96112c82facd5e09602
lender512 <52045791+lender512@users.noreply.github.com>
2020-04-11 23:05:48 -0500

    Add files via upload

---
+++ Semana01B.cpp
@@ -0,0 +1,110 @@
+﻿#include <iostream>
+#include <cassert>
+#include <string>
+#include "linkedList.h"
+
+using namespace std;
+
+int main() {
+
+	utec::linked_list_t ll;
+
+
+	// Probar el metodo push_front;
+	for (int i = 0; i < 10; ++i) {
+		ll.push_front(i);
+	}
+	
+	// Verifico el tamano
+	assert(ll.size() == 10);
+	
+	string result;
+
+	for (int i = 0; i < ll.size(); ++i) {
+		result += to_string(ll.item(i)) + " ";
+	}
+	assert(result == "9 8 7 6 5 4 3 2 1 0 ");
+
+	// Probar el push_back
+	for (int i = 0; i < 5; ++i) {
+		ll.push_back(i);
+	}
+
+	assert(ll.size() == 15);
+	
+	result = "";
+	
+	for (int i = 0; i < ll.size(); ++i) {
+		result += to_string(ll.item(i)) + " ";
+	}
+	assert(result == "9 8 7 6 5 4 3 2 1 0 0 1 2 3 4 ");
+
+	
+
+	// Probar el metodo pop_front;
+	for (int i = 0; i < 5; ++i) {
+		ll.pop_front();
+	}
+
+	result = "";
+
+	for (int i = 0; i < ll.size(); ++i) {
+		result += to_string(ll.item(i)) + " ";
+	}
+	assert(result == "4 3 2 1 0 0 1 2 3 4 ");
+
+	// Verifico el tamano
+	assert(ll.size() == 10);
+
+
+	// Probar el metodo pop_back;
+	for (int i = 0; i < 5; ++i) {
+		ll.pop_back();
+	}
+
+	result = "";
+
+	for (int i = 0; i < ll.size(); ++i) {
+		result += to_string(ll.item(i)) + " ";
+	}
+	assert(result == "4 3 2 1 0 ");
+
+	// Verifico el tamano
+	assert(ll.size() == 5);
+
+	//Probar erase
+	ll.erase(2);
+
+	result = "";
+
+	for (int i = 0; i < ll.size(); ++i) {
+		result += to_string(ll.item(i)) + " ";
+	}
+	assert(result == "4 3 1 0 ");
+
+	// Verifico el tamano
+	assert(ll.size() == 4);
+
+	//Probar insert
+	ll.insert(2, 100);
+
+	result = "";
+
+	for (int i = 0; i < ll.size(); ++i) {
+		result += to_string(ll.item(i)) + " ";
+	}
+	assert(result == "4 3 100 1 0 ");
+
+	// Verifico el tamano
+	assert(ll.size() == 5);
+
+
+
+	
+	
+
+
+
+	std::cout << "Ejecucion exitosa!!" << std::endl;
+	return 0;
+}
---
+++ linkedList.cpp
@@ -0,0 +1,186 @@
+//
+// Created by utec on 4/11/20.
+//
+
+#include "linkedList.h"
+
+
+utec::first::linked_list_t::linked_list_t(const utec::linked_list_t& other) {
+
+}
+
+utec::linked_list_t& utec::first::linked_list_t::operator=(const utec::linked_list_t& other) {
+
+	// TO DO
+
+	return *this;
+}
+
+utec::first::linked_list_t::linked_list_t(utec::linked_list_t&& other) noexcept {
+
+}
+
+utec::linked_list_t& utec::first::linked_list_t::operator=(utec::linked_list_t&& other) noexcept {
+
+	// TO DO
+	return *this;
+}
+
+utec::first::linked_list_t::~linked_list_t() {
+
+}
+
+void utec::first::linked_list_t::push_front(int value) {
+
+	/*
+	auto node = new node_t{value, head_}
+	head_ = node;
+	*/
+	head_ = new node_t{ value, head_ };
+	if (tail_ == nullptr) tail_ = head_;
+	size_++;
+
+}
+
+void utec::first::linked_list_t::push_back(int value) {
+	
+	auto node = new node_t{ value, nullptr };
+	if (head_ == nullptr) head_ = node;
+	else {
+		tail_->next_ = node;
+	}
+	tail_ = node;
+	
+	size_++;
+}
+
+void utec::first::linked_list_t::insert(size_t index, int value) {
+	if (index == 0) {
+		//hace pushfront
+		head_ = new node_t{ value, head_ };
+		if (tail_ == nullptr) tail_ = head_;
+		size_++;
+	}
+	else if (index == size_ - 1) {
+		//hace pushback
+		auto node = new node_t{ value, nullptr };
+		if (head_ == nullptr) head_ = node;
+		else {
+			tail_->next_ = node;
+		}
+		tail_ = node;
+
+		size_++;
+	}
+	else {
+		auto start = head_;
+		for (int i = 0; i < index - 1; i++) {
+			start = start->next_;
+		}
+		auto temp = start->next_;
+		start->next_ = new node_t{ value, temp };
+		//delete temp;
+		size_++;
+	}
+
+}
+
+void utec::first::linked_list_t::pop_front() {
+	if (head_ == tail_) {
+		delete head_;
+		head_ = tail_ = nullptr;
+		size_ = 0;
+	}
+	else {
+		auto next = head_->next_;
+		delete head_;
+		head_ = next;
+		size_--;
+	}
+}
+
+void utec::first::linked_list_t::pop_back() {
+	if (head_ == tail_) {
+		delete head_;
+		head_ = tail_ = nullptr;
+		size_ = 0;
+	}
+	else {
+		auto i = head_;
+		while (i->next_->next_ != NULL) {
+			i = i->next_;
+		}
+		delete tail_;
+		tail_ = i;
+		tail_->next_ = nullptr;
+		size_--;
+	}
+}
+
+void utec::first::linked_list_t::erase(size_t index) {
+	if (index >= size_) {
+		std::cout << "fuera de rango";
+	}
+	else if (index == 0) {
+		//hace popfront
+		if (head_ == tail_) {
+			delete head_;
+			head_ = tail_ = nullptr;
+			size_ = 0;
+		}
+		else {
+			auto next = head_->next_;
+			delete head_;
+			head_ = next;
+			size_--;
+		}
+	}
+	else if (index == size_-1){
+		//hace popback
+		if (head_ == tail_) {
+			delete head_;
+			head_ = tail_ = nullptr;
+			size_ = 0;
+		}
+		else {
+			auto i = head_;
+			while (i->next_->next_ != NULL) {
+				i = i->next_;
+			}
+			delete tail_;
+			tail_ = i;
+			tail_->next_ = nullptr;
+			size_--;
+		}
+	}
+	else {
+		auto start = head_;
+		for (int i = 0; i < index - 1; i++) {
+			start = start->next_;
+		}
+		auto temp = start->next_->next_;
+		delete start->next_;
+		start->next_ = temp;
+		//delete temp;
+		size_--;
+	}
+	
+}
+
+int& utec::first::linked_list_t::item(size_t index) {
+	auto actual = head_;
+	while (index--)
+		actual = actual->next_;
+	return actual->value_;
+}
+
+const int& utec::first::linked_list_t::item(size_t index) const {
+	auto actual = head_;
+	while (index--)
+		actual = actual->next_;
+	return actual->value_;
+}
+
+size_t utec::first::linked_list_t::size() const {
+	return size_;
+}
